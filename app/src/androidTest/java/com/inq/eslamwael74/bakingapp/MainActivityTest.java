a4b5353cb4479e289a75cdbe438675c7d2866bd8
Eslam Wael <e.wael@4artstudio.net>
2017-11-14 23:25:49 +0200

    HelloFromHere

---
+++ app/src/androidTest/java/com/inq/eslamwael74/bakingapp/MainActivityTest.java
@@ -0,0 +1,41 @@
+package com.inq.eslamwael74.bakingapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.inq.eslamwael74.bakingapp.Activity.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Eslam Wael on 11/14/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule public ActivityTestRule<MainActivity> mActivtyTestRule
+            = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+
+    @Test
+    public void checkOnMain(){
+        //find the view and perform; action on the view
+        onView((withId(R.id.bar_text))).perform(click());
+
+        //check what is expected
+        onView(withId(R.id.bar_text)).check(matches(withText("BakingApp")));
+    }
+
+
+
+
+}
